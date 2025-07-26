d2386331544d2a0b7c7430f5ced72c2d8b2c50a5
Suhas <suhas0168@gmail.com>
2025-07-26 12:31:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const typed = new Typed(".text", {
+  strings: [
+    "Frontend Developer",
+    "Web Designer",
+    "UI/UX Designer",
+    "Python Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  backDelay: 1000,
+  loop: true,
+});
